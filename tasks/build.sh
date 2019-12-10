05999a98efa3dc11939cbf4f9330ba8616a86aec
Kris De Volder <kdevolder@pivotal.io>
2019-12-10 13:10:21 -0800

    Fix problem in build script

--- tasks/build.sh
+++ tasks/build.sh
@@ -3,4 +3,4 @@ set -e
 workdir=$(pwd)
 cd $workdir/source-git
 ./mvnw -Dmaven.test.skip=true clean install
-cp start-site/target/sprotty-si-view-*-exec.jar $workdir/output/app.jar
+cp sprotty-si-view/target/sprotty-si-view-*-exec.jar $workdir/output/app.jar
